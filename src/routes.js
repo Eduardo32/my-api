ba549bb2eb7d334dc4e6b5b11cfba29f93068c0e
Eduardo32 <p.eduardo.32@gmail.com>
2020-11-25 09:48:00 -0300

    Adicionando rota '/' com link do repositorio no git

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const UserController = require('./controllers/UserController');
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+    res.send('<a href="https://github.com/Eduardo32/my-api" target="_blank" >github.com/Eduardo32/my-api</a>');
+});
+
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
